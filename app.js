36cd139ac732c16f2f2c7f57b723f94551d0af44
111 <1192439205@qq.com>
2018-12-14 13:22:28 +0800

    代码初始化

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require("express")
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("ok")
+})
+
+app.listen(80,()=>{
+    console.log("server running at http://127.0.0.1")
+})
